19031e43bc147919833bb0550301856d454c8c94
Mccaryr <robert.mccary@yahoo.com>
2019-07-08 08:10:51 -0700

    Setup Environment

---
+++ app/controllers/sessions_controller.rb
@@ -0,0 +1,19 @@
+# class SessionsController < ApplicationController
+#     before_action :current_user, only: [:show]
+#     def new 
+#         @user = User.new 
+#     end 
+
+#     def create  
+#         @user = User.find_or_create_by(user_params)
+#         if @user && @user.authenticate(params[:password])
+#             session[:user_id] = @user.id 
+#             redirect_to root_path
+#         else 
+#             redirect_to signin_path 
+#         end 
+
+        # def user_params 
+        #     params.require(:user).permit(:name,:password, :uid, :email, :name)
+        # end 
+#     end
---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,29 @@
+class UsersController < ApplicationController 
+    before_action :current_user, only: [:show]
+    before_action :require_login, only: [:show]
+    
+    
+    def new 
+        @user = User.new
+    end 
+
+    def create
+        @user = User.new(user_params)
+        if @user.save 
+            session[:user_id] = @user.id 
+            redirect_to root_path 
+        else 
+            render new_user_path 
+        end 
+    end 
+
+    def show 
+        @user = User.find_by_id(params[:id])
+    end     
+
+    private 
+
+    def user_params
+        params.require(:user).permit(:name, :password)
+    end 
+end 

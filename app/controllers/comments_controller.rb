4a3a3760955211a4254fb9b10a1d41e54a42a112
Mccaryr <robert.mccary@yahoo.com>
2019-07-20 07:42:52 -0700

    Personalized Comments

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -21,6 +21,7 @@ class CommentsController < ApplicationController
     def create
         @post = Post.find(params[:post_id])
         @comment = @post.comments.build(comment_params)
+        @comment.user = current_user 
         if @comment.save
             redirect_to post_comments_path
         else
@@ -83,7 +84,7 @@ class CommentsController < ApplicationController
 private 
 
     def comment_params 
-        params.require(:comment).permit(:post_id, :content, :user_id)
+        params.require(:comment).permit(:post_id, :content, :user_id, :username)
     end 
 
     def set_post 
